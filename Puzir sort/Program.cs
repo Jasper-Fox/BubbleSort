f699239872b0279a3e4bd4a14e2088048940b441
Jasper-Fox <azimo74@bk.ru>
2024-02-16 17:39:03 +0300

    Add File

---
+++ Puzir sort/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Puzir_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int[] A = new int[20];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+            A[i] = rand.Next(1, 100);
+                Console.Write($"{A[i]} ");
+            }
+            Console.WriteLine("\n");
+            PSort(A);
+            foreach (int a in A)
+                Console.Write($"{a} ");         
+        }
+
+        static int[] PSort(int[] X)
+        {
+            for (int j = 0; j < X.Length; j++)          
+                for (int i = X.Length - 1; i > j; i--)
+                    if (X[i] < X[i - 1])
+                    {
+                        int a = X[i];
+                        X[i] = X[i - 1];
+                        X[i - 1] = a;
+                    }                                           
+            return X;
+        }
+    }
+}
